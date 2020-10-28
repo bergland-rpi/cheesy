3f9d3bc45dfaa45db7525fa77a24caef82d00fb5
bergland-rpi <bergland-rpi@gmail.com>
2020-10-27 21:03:41 -0400

    changes

--- alan_pull_restart.sh
+++ alan_pull_restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sleep 30
-hn=$(hostname)
+hn=cheesy
 cd /home/pi/$hn
 git pull
 
@@ -12,12 +12,5 @@ else
     echo "Stopped"
     sudo python /home/pi/$hn/masterprogram.py /home/pi/$hn/config.txt &
     sleep 15
-    if pgrep -f "masterprogram.py" > /dev/null
-    then
-    	sudo python /home/pi/$hn/restart_email.py
-	echo 'program restarted'
-    else
-    	sudo python /home/pi/$hn/restart_fail_email.py
-	echo 'program did not start'
-    fi
+    
 fi
